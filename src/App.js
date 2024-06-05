6e0fc4793e7fd8fffafdf0b5551e315d84dc5b33
victorydavidson <victorydavidson19@gmail.com>
2024-06-05 20:29:29 +0100

    Update package.json, index.html, App.js, and index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Menu from './components/Menu';
 import About from './components/About';
@@ -11,39 +11,47 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router>
-    <BackgroundContainer>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/see-you-soon" element={<SeeYouSoon />} />
-        <Route path="/appetizers" element={<Appetizers />} />
+    <HashRouter>
+      <BackgroundContainer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/see-you-soon" element={<SeeYouSoon />} />
+          <Route path="/appetizers" element={<Appetizers />} />
           <Route path="/main-dishes" element={<MainDishes />} />
           <Route path="/desserts" element={<Desserts />} />
-      </Routes>
-    </BackgroundContainer>
-  </Router>
-);
+        </Routes>
+      </BackgroundContainer>
+    </HashRouter>
+  );
 };
 
 const BackgroundContainer = ({ children }) => {
-const location = useLocation();
+  const location = useLocation();
 
-let backgroundClass = '';
-if (location.pathname === '/') {
-  backgroundClass = 'home-background';
-} else if (location.pathname === '/menu') {
-  backgroundClass = 'menu-background';
-} else if (location.pathname === '/about') {
-  backgroundClass = 'about-background';
-}
+  let backgroundClass = '';
+  if (location.pathname === '/') {
+    backgroundClass = 'home-background';
+  } else if (location.pathname === '/menu') {
+    backgroundClass = 'menu-background';
+  } else if (location.pathname === '/about') {
+    backgroundClass = 'about-background';
+  } else if (location.pathname === '/see-you-soon') {
+    backgroundClass = 'see-you-soon-background';
+  } else if (location.pathname === '/appetizers') {
+    backgroundClass = 'appetizers-background';
+  } else if (location.pathname === '/main-dishes') {
+    backgroundClass = 'main-dishes-background';
+  } else if (location.pathname === '/desserts') {
+    backgroundClass = 'desserts-background';
+  }
 
-return (
-  <div className={backgroundClass}>
-    {children}
-  </div>
-);
+  return (
+    <div className={backgroundClass}>
+      {children}
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <HashRouter> {/* Use HashRouter instead of BrowserRouter */}
     <App />
-  </React.StrictMode>
+  </HashRouter>
+</React.StrictMode>
 );
